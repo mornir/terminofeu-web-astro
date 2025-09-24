82c00d4d1d33a9180c678b2c848a96b465d8b1fa
mornir <jeromepott@hotmail.com>
2025-09-24 15:09:31 +0200

    feat: add i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,41 @@ import alpinejs from '@astrojs/alpinejs'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import { i18n, filterSitemapByDefaultLocale } from 'astro-i18n-aut/integration'
+
 import icon from 'astro-icon'
+
+import sitemap from '@astrojs/sitemap'
+
+const defaultLocale = 'de'
+const locales = {
+  de: 'de-CH', // the `defaultLocale` value must present in `locales` keys
+  fr: 'fr-CH',
+  it: 'it-CH',
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), icon()],
-
+  site: 'https://terminofeu.ch/',
+  trailingSlash: 'always',
+  build: {
+    format: 'directory',
+  },
+  integrations: [
+    alpinejs(),
+    icon(),
+    i18n({
+      locales,
+      defaultLocale,
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: filterSitemapByDefaultLocale({ defaultLocale }),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
